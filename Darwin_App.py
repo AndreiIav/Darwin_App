72c58b00e72ac8d64f37102eeb93cbfa1e2293ca
Andrei <Andrey@yahoo.com>
2022-12-11 20:03:44 +0200

    Added logic to connect to db and return the count of the searched word.

--- Darwin_App.py
+++ Darwin_App.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 
 app = Flask(__name__)
@@ -10,4 +11,14 @@ def search_page():
         return render_template("search_page.html")
     if request.method == "POST":
         s_word = request.form.get("search_term", "default")
-        return render_template("search_page.html", searched=s_word)
+
+        conn = sqlite3.connect("test.db")
+        c = conn.cursor()
+        c.execute(
+            "SELECT COUNT(*) FROM magazine_number_content WHERE magazine_content LIKE :word",
+            {"word": "%" + s_word + "%"},
+        )
+        res = c.fetchone()[0]
+        conn.close()
+
+        return render_template("search_page.html", searched=s_word, res=res)
