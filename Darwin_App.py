19c8254c177a620932196d20e9f1962a9d52679e
Andrei <Andrey@yahoo.com>
2022-12-11 11:12:55 +0200

    Added first version of base.html and search_page.html .

--- Darwin_App.py
+++ Darwin_App.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def search_page():
-    return f"Search Page"
+    return render_template("search_page.html")
