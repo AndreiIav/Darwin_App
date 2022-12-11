37daef7f36f4c4f0672f816a293e135a807ae2a3
Andrei <Andrey@yahoo.com>
2022-12-11 13:16:01 +0200

    First Commit.

--- Darwin_App.py
+++ Darwin_App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def search_page():
     return render_template("search_page.html")
+
+
+@app.route("/searched_word/<searched_word>")
+def searched_word(searched_word):
+    return render_template("searched_word.html", searched=searched_word)
