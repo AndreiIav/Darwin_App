11a4ea4d2d0f78da5233fc95b7b22fa00a182e2f
Andrei <Andrey@yahoo.com>
2022-12-11 16:48:44 +0200

    Added logic for POST method in / route.

--- Darwin_App.py
+++ Darwin_App.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=("GET", "POST"))
 def search_page():
-    return render_template("search_page.html")
-
-
-@app.route("/searched_word", methods=["POST"])
-def searched_word():
-    s_word = request.form.get("search_term", "default")
-    return render_template("searched_word.html", searched=s_word)
+    if request.method == "GET":
+        return render_template("search_page.html")
+    if request.method == "POST":
+        s_word = request.form.get("search_term", "default")
+        return render_template("searched_word.html", searched=s_word)
