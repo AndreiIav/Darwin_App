d93c4fe096912ef1a76b09cf39c718a3d9c19268
Andrei <Andrey@yahoo.com>
2022-12-18 13:16:10 +0200

    Added details of the request to be displayed as JSON. WIP.

--- Darwin_App.py
+++ Darwin_App.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from logic import get_count
+from flask import Flask, render_template, request, make_response
+from logic import get_count, get_details_for_searched_term
 
 
 app = Flask(__name__)
@@ -11,8 +11,20 @@ def search_page():
         return render_template("search_page.html")
     if request.method == "POST":
         s_word = request.form.get("search_term", "default")
-        return render_template(
-            "search_page.html",
-            searched=s_word,
-            res=get_count(s_word=s_word),
-        )
+
+        headers = {"Content-Type": "application/json"}
+        result_list = get_details_for_searched_term(s_word=s_word)
+
+        response = []
+        for res in result_list:
+
+            partial_values = {}
+            partial_values["magazine_name"] = res[0]
+            partial_values["magazine_year"] = res[1]
+            partial_values["magazine_number"] = res[2]
+            partial_values["page"] = res[3]
+            partial_values["link"] = res[4]
+
+            response.append(partial_values)
+
+        return make_response(response, 200, headers)
--- logic.py
+++ logic.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import get_db
 
 
@@ -11,3 +10,21 @@ def get_count(s_word):
     ).fetchone()[0]
 
     return res
+
+
+def get_details_for_searched_term(s_word):
+
+    db = get_db()
+    all_res = db.execute(
+        """
+          SELECT DISTINCT m.name, ma.year, mn.magazine_number, mnc.magazine_page, mn.magazine_number_link
+          FROM magazines m
+          INNER JOIN magazine_year ma ON m.id = ma.magazine_id
+          INNER JOIN magazine_number mn ON ma.id = mn.magazine_year_id
+          INNER JOIN magazine_number_content mnc ON mn.id = mnc.magazine_number_id
+          WHERE mnc.magazine_content LIKE :word
+        """,
+        {"word": "%" + s_word + "%"},
+    ).fetchall()
+
+    return all_res
