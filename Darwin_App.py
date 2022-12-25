5bdb2b8d5d018ef430f3ef0b956e54f1f54a0e1f
Andrei <Andrey@yahoo.com>
2022-12-25 12:21:18 +0200

    Added render_template for /search endpoint. WIP.

--- Darwin_App.py
+++ Darwin_App.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def search_page():
-    return render_template("search_page.html")
+    return render_template("base.html")
 
 
 @app.route("/search", methods=["POST"])
@@ -15,7 +15,11 @@ def search_for_term():
 
     s_word = request.form.get("search_term", "default")
 
-    headers = {"Content-Type": "application/json"}
+    # headers = {"Content-Type": "application/json"}
     result_list = get_json_details_for_searched_term(s_word=s_word)
 
-    return make_response(result_list, 200, headers)
+    # return make_response(result_list, 200, headers)
+
+    return render_template(
+        "search_page.html", result_list=result_list, searched_term=s_word
+    )
