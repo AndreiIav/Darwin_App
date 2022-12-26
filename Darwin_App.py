83086942b1f85e56e8578d816ec1e09b44c76a26
Andrei <Andrey@yahoo.com>
2022-12-26 09:50:01 +0200

    Added loop for result_list in search_page + other minor changes.

--- Darwin_App.py
+++ Darwin_App.py
@@ -10,8 +10,11 @@ def search_page():
     return render_template("base.html")
 
 
-@app.route("/search", methods=["POST"])
+@app.route("/search", methods=["GET", "POST"])
 def search_for_term():
+
+    if request.method == "GET":
+        return render_template("base.html")
 
     s_word = request.form.get("search_term", "default")
 
