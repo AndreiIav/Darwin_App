0c3c42dd1009016f1a96a3727afa1b1616237dc4
Andrei <Andrey@yahoo.com>
2022-12-27 11:15:34 +0200

    Added forms.py with SearchForm class.

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField                # field types
+from wtforms.validators import DataRequired, Length         # validators
+
+
+class SearchForm(FlaskForm):
+
+    search_box = StringField(
+        "Search Box",
+        [
+            DataRequired(),
+            Length(min=4),
+            message=('The searched term must be greater than 3 characters.')
+        ]
+    )
+    submit = SubmitField('Search')
