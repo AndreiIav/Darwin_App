144c8bcb09a3343c51d5da0f591061938517be39
Andrei <Andrey@yahoo.com>
2022-12-27 18:25:27 +0200

    Added logic for forms in base.html and Darwin_App.py .

--- Darwin_App.py
+++ Darwin_App.py
@@ -1,28 +1,36 @@
 from flask import Flask, render_template, request, make_response
 from logic import get_json_details_for_searched_term
+from forms import SearchForm
 
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "random key"
+
 
 @app.route("/")
 def search_page():
-    return render_template("base.html")
+    search_form = SearchForm()
+    return render_template("base.html", search_form=search_form)
 
 
 @app.route("/search", methods=["GET", "POST"])
 def search_for_term():
 
-    if request.method == "GET":
-        return render_template("base.html")
+    search_form = SearchForm()
 
-    s_word = request.form.get("search_term", "default")
-    result_list = get_json_details_for_searched_term(s_word=s_word)
-    results_count = len(result_list)
+    if request.method == "POST":
 
-    return render_template(
-        "search_page.html",
-        result_list=result_list,
-        searched_term=s_word,
-        results_count=results_count,
-    )
+        s_word = search_form.search_box.data
+        result_list = get_json_details_for_searched_term(s_word=s_word)
+        results_count = len(result_list)
+
+        return render_template(
+            "search_page.html",
+            result_list=result_list,
+            searched_term=s_word,
+            results_count=results_count,
+            search_form=search_form,
+        )
+
+    return render_template("base.html", search_form=search_form)
