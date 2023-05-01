040883e8c8ec1933db5a37298654da814cafe7dc
Andrei <Andrey@yahoo.com>
2023-05-01 09:38:10 +0300

    Moved (and enhanced) the logic from '/show_page' route for highlighting the searched term to separate functions in logic.py module.

--- application/search_page/logic.py
+++ application/search_page/logic.py
@@ -101,3 +101,90 @@ def get_magazine_content_details(page_id):
     ).filter(MagazineNumberContentFTS.rowid == page_id)
 
     return magazine_content_details[0][0]
+
+
+def get_content_string_length(s_word):
+    """
+    A function that accepts the searched term (s_word) and returns its length.
+    """
+
+    return len(s_word)
+
+
+def get_indexes_for_highlighting_s_word(s_word, content, content_string_length):
+    """
+    A function that accepts the searched term (s_word, string) and the content of show_page view
+    as string (content, string).
+    It returns a list of all the start indexes (int) where the searched term is found in content.
+    """
+
+    convert_romanian_characters_to_english_dict = {
+        259: 97,  # ă -> a
+        226: 97,  # â -> a
+        238: 105,  # î -> i
+        351: 115,  # ş -> s
+        537: 115,  # ș -> s
+        539: 116,  # ț -> t
+        355: 116,  # ţ -> t
+    }
+
+    content_string_length = content_string_length
+    formated_content_string = content.lower().translate(
+        convert_romanian_characters_to_english_dict
+    )
+    formated_s_word = s_word.lower().translate(
+        convert_romanian_characters_to_english_dict
+    )
+
+    indexes_for_highlighting_s_word = []
+    find_s_word = formated_content_string.find(formated_s_word)
+
+    while find_s_word > -1:
+        indexes_for_highlighting_s_word.append(find_s_word)
+        current_last_index = indexes_for_highlighting_s_word[-1]
+        index_content_string = formated_content_string[
+            current_last_index + content_string_length :
+        ]
+
+        find_s_word = index_content_string.find(formated_s_word)
+        if find_s_word > -1:
+            find_s_word = current_last_index + content_string_length + find_s_word
+
+    return indexes_for_highlighting_s_word
+
+
+def get_distinct_s_words_variants(
+    indexes_for_highlighting_s_word, content, content_string_length
+):
+    """
+    A function that accepts a list of indexes (indexes_for_highlighting_s_word, int), the
+    content of show_page view as string (content, string) and the length of the searched term
+    (content_string_length, int).
+    It returns a list of distinct versions (list of strings) (i, e.: "Darwin", "darwin" or 'Babeș', 'babeș')
+    of the searched term.
+    """
+
+    distinct_s_words_variants = []
+
+    for index in indexes_for_highlighting_s_word:
+        string_to_search = content[index : index + content_string_length]
+        if string_to_search not in distinct_s_words_variants:
+            distinct_s_words_variants.append(string_to_search)
+
+    return distinct_s_words_variants
+
+
+def add_html_mark_tags_to_the_searched_term(distinct_s_words_variants, content):
+    """
+    A function that accepts a list of distinct versions of the searched term (list of strings)
+    and the content of show_page view as string (content, string).
+    It returns the content of show_page view as string with <mark> tags around all versions of
+    the searched term
+    """
+
+    while len(distinct_s_words_variants) > 0:
+        word = distinct_s_words_variants.pop()
+
+        content = content.replace(word, "<mark>" + word + "</mark>")
+
+    return content
--- application/search_page/search_page.py
+++ application/search_page/search_page.py
@@ -7,6 +7,10 @@ from .logic import (
     get_distinct_magazine_names_and_count_for_searched_term,
     format_search_word,
     get_magazine_content_details,
+    get_indexes_for_highlighting_s_word,
+    get_content_string_length,
+    get_distinct_s_words_variants,
+    add_html_mark_tags_to_the_searched_term,
 )
 
 
@@ -57,34 +61,15 @@ def display_magazine_content():
     page_id = request.args.get("page_id")
 
     content = get_magazine_content_details(page_id)
-
-    convert_romanian_characters_to_english_dict = {
-        259: 97,  # ă -> a
-        226: 97,  # â -> a
-        238: 105,  # î -> i
-        351: 115,  # ş -> s
-        537: 115,  # ș -> s
-        539: 116,  # ț -> t
-        355: 116,  # ţ -> t
-    }
-
-    content_string_length = len(s_word)
-    formated_content_string = content.lower().translate(
-        convert_romanian_characters_to_english_dict
+    content_string_length = get_content_string_length(s_word)
+    indexes_for_highlighting_s_word = get_indexes_for_highlighting_s_word(
+        s_word, content, content_string_length
     )
-    find_s_word = formated_content_string.find(
-        s_word.lower().translate(convert_romanian_characters_to_english_dict)
+    distinct_s_words_variants = get_distinct_s_words_variants(
+        indexes_for_highlighting_s_word, content, content_string_length
+    )
+    content = add_html_mark_tags_to_the_searched_term(
+        distinct_s_words_variants, content
     )
 
-    if find_s_word > -1:
-        substring_to_be_highlighted = content[
-            find_s_word : find_s_word + content_string_length
-        ]
-        content = Markup(
-            content.replace(
-                substring_to_be_highlighted,
-                "<mark>" + substring_to_be_highlighted + "</mark>",
-            )
-        )
-
-    return content
+    return Markup(content)
