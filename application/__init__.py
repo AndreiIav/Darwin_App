4499a2b969bc7741749d4926f6ba08a86f200da3
Andrei <Andrey@yahoo.com>
2023-05-28 12:38:03 +0300

    Updated app.config from file to object.

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from os import path
-from pathlib import Path
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,7 +7,7 @@ db = SQLAlchemy()
 def init_app():
 
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_pyfile(path.join(Path.cwd(), "config.py"))
+    app.config.from_object("config.DevelopmentConfig")
 
     db.init_app(app)
 
