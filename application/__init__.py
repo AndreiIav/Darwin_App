8571f36abd3c821f811037de12d174f6cbcbf9cd
Andrei <Andrey@yahoo.com>
2023-09-10 12:11:57 +0300

    Modified how the application factory is structured, by using helper functions initialize extensions and register blueprints, and added __init__ files in search_page and home folders.

--- application/__init__.py
+++ application/__init__.py
@@ -1,25 +1,47 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+# -------------
+# Configuration
+# -------------
 
 db = SQLAlchemy()
+
+# ----------------------------
+# Application Factory Function
+# ----------------------------
 
 
 def init_app():
 
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__)
+
+    # Configure the Flask application
     config_type = os.getenv("CONFIG_TYPE", default="config.DevelopmentConfig")
     app.config.from_object(config_type)
 
+    initialize_extensions(app)
+    register_blueprints(app)
+
+    return app
+
+
+# ----------------
+# Helper Functions
+# ----------------
+
+
+def register_blueprints(app):
+    # Import the blueprints
+    from application.home.home import home_bp
+    from application.search_page.search_page import search_page_bp
+
+    # Register Blueprints
+    app.register_blueprint(home_bp)
+    app.register_blueprint(search_page_bp)
+
+
+def initialize_extensions(app):
     db.init_app(app)
-
-    with app.app_context():
-
-        from .home import home
-        from .search_page import search_page
-
-        # Register Blueprints
-        app.register_blueprint(home.home_bp)
-        app.register_blueprint(search_page.search_page_bp)
-
-        return app
