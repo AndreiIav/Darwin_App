9dfd9b8544b4e52f0b947ded1c9a0592a730b028
Andrei <Andrey@yahoo.com>
2023-09-10 19:14:34 +0300

    Added no_results_found.html template and logic in /search route to trigger it: if the search_term has less than four characters or if no results were found.

--- application/search_page/search_page.py
+++ application/search_page/search_page.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from flask import render_template, request, session, current_app
-
-from .logic import (
+from application.search_page.logic import (
     get_details_for_searched_term,
     get_details_for_searched_term_for_specific_magazine,
     get_distinct_magazine_names_and_count_for_searched_term,
@@ -29,6 +28,9 @@ def search_for_term():
     request_s_word = request.args.get("search_box")
     request_s_word = format_search_word(request_s_word)
     s_word = store_s_word_in_session(session_s_word, request_s_word)
+    if len(s_word) < 4:
+        return render_template("no_results_found.html", not_minimum_s_word_length=True)
+
     preview_length = current_app.config["PREVIEW_SUBSTRING_LENGTH"]
     page = request.args.get("page", 1, type=int)
 
@@ -43,6 +45,9 @@ def search_for_term():
     details_for_searched_term = get_details_for_searched_term(
         formatted_s_word=formatted_s_word
     )
+
+    if len(list(details_for_searched_term)) == 0:
+        return render_template("no_results_found.html", searched_term=s_word)
 
     magazine_filter = request.args.get("magazine_filter")
 
