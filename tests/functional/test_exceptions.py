5472a20b95e631e23a37eb691830a903ec028bf6
Andrei <Andrey@yahoo.com>
2023-12-17 12:24:33 +0200

    Added tests for /contact and /about pages, and for exceptions.

---
+++ tests/functional/test_exceptions.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_404_exception_in_magazine_details_page(test_client):
+    response = test_client.get("/magazine_details", query_string={"magazine_id": "0"})
+
+    assert response.status_code == 404
+
+@pytest.mark.parametrize("incorrect_input", ["a", "abc","a" * 201])
+def test_no_results_found_in_search_page_due_to_incorrect_lenghth_of_input(test_client, incorrect_input):
+    response = test_client.get("/results/search", query_string={"search_box": incorrect_input})
+
+    assert response.status_code == 200
+    assert b"No results found." in response.data
+    assert b"The search term can have at least 4 characters and at most 200." in response.data
+
+def test_no_results_found_in_search_page_due_to_not_found_term(test_client):
+    term = "supercalifragilistic"
+    response = test_client.get("/results/search",query_string={"search_box": term})
+    
+    assert response.status_code == 200
+    assert b"No results found." in response.data
+    assert b"No results were found for" in response.data
+    assert term.encode() in response.data
--- tests/functional/test_home_bp.py
+++ tests/functional/test_home_bp.py
@@ -1,15 +1,9 @@
+
 # Tests for "/" page
 def test_home_page_get(test_client):
     response = test_client.get("/")
 
     assert response.status_code == 200
-
-
-def test_home_page_post(test_client):
-    response = test_client.post("/")
-
-    assert response.status_code == 405
-
 
 # Tests for "/magazine_details" page
 def test_magazine_details_page_get(test_client):
@@ -17,3 +11,15 @@ def test_magazine_details_page_get(test_client):
 
     assert response.status_code == 200
     assert b"Albina (1866-1876)" in response.data
+
+# Tests for "/contact" page
+def test_contact_page_get(test_client):
+    response = test_client.get("/contact")
+
+    assert response.status_code == 200
+
+# Tests for "/about" page
+def test_about_page_get(test_client):
+    response = test_client.get("/about")
+
+    assert response.status_code == 200
