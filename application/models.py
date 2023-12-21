771b1f4fd6b1b06d3a0bf436a533d8365b64224b
Andrei <Andrey@yahoo.com>
2023-12-21 09:24:15 +0200

    Updated __repr__() for all models.

--- application/models.py
+++ application/models.py
@@ -11,7 +11,7 @@ class Magazines(db.Model):
     magazine_link = db.Column(db.Text)
 
     def __repr__(self):
-        return f"Magazine name: {self.name}, Magazine Link: {self.magazine_link}"
+        return f"Magazines(id={self.id},name={self.name},magazine_link={self.magazine_link})"
 
 
 class MagazineYear(db.Model):
@@ -24,10 +24,8 @@ class MagazineYear(db.Model):
     magazine_year_link = db.Column(db.Text)
 
     def __repr__(self):
-        return (
-            f"Magazine Year: {self.year}, Magazine_Year_Link: {self.magazine_year_link}"
-        )
-
+        return f"MagazineYear(id={self.id},magazine_id={self.magazine_id},year={self.year},magazine_year_link={self.magazine_year_link})"
+        
 
 class MagazineNumber(db.Model):
 
@@ -39,7 +37,7 @@ class MagazineNumber(db.Model):
     magazine_number_link = db.Column(db.Text)
 
     def __repr__(self):
-        return f"Magazine Number: {self.magazine_number}, Magazine Number Link: {self.magazine_number_link}"
+        return f"MagazineNumber(id={self.id},magazine_year_id={self.magazine_year_id},magazine_number={self.magazine_number},magazine_number_link={self.magazine_number_link})"
 
 
 class MagazineNumberContent(db.Model):
@@ -52,7 +50,7 @@ class MagazineNumberContent(db.Model):
     magazine_page = db.Column(db.Text)
 
     def __repr__(self):
-        return f"Magazine Content: {self.magazine_content}, Magazine Page: {self.magazine_page}"
+        return f"MagazineNumberContent(id={self.id},magazine_number_id={self.magazine_number_id},magazine_content={self.magazine_content},magazine_page={self.magazine_page})"
 
 
 class MagazineNumberContentFTS(db.Model):
@@ -61,3 +59,6 @@ class MagazineNumberContentFTS(db.Model):
 
     rowid = db.Column(db.Integer, primary_key=True)
     magazine_content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"MagazineNumberContentFTS(rowid={self.rowid},magazine_content={self.magazine_content})"
