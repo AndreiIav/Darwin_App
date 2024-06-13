02ed418e21520f1c915a7b085a402af1716ac652
Andrei <Andrey@yahoo.com>
2024-06-13 08:51:16 +0300

    Added tests for get_details_for_searched_term_for_specific_magazine

--- tests/functional/test_search_page_bp.py
+++ tests/functional/test_search_page_bp.py
@@ -1,5 +1,13 @@
 import pytest
 
+import flask_sqlalchemy
+
+from application.search_page.logic import (
+    get_details_for_searched_term,
+    get_details_for_searched_term_for_specific_magazine,
+)
+
+
 # Tests for /results/
 def test_results_page_get(test_client):
 
@@ -10,12 +18,16 @@ def test_results_page_get(test_client):
     assert b"Go back to home page" in response.data
     assert b"Number of results displayed by magazine name" in response.data
 
-def test_results_page_without_magazine_filter_go_back_to_all_results_button_not_displayed(test_client):
+
+def test_results_page_without_magazine_filter_go_back_to_all_results_button_not_displayed(
+    test_client,
+):
 
     s_word = "Victor+Babeș"
     response = test_client.get("/results/search", query_string={"search_box": s_word})
 
     assert b"Go back to all results" not in response.data
+
 
 def test_results_page_post(test_client):
 
@@ -24,7 +36,10 @@ def test_results_page_post(test_client):
 
     assert response.status_code == 405
 
+
 results_page_pages = [1, 2, 23]
+
+
 @pytest.mark.parametrize("pages", results_page_pages)
 def test_results_page_pagination_existent_page(test_client, pages):
 
@@ -36,6 +51,7 @@ def test_results_page_pagination_existent_page(test_client, pages):
 
     assert response.status_code == 200
 
+
 def test_results_page_pagination_not_existent_page(test_client):
 
     s_word = "Victor+Babeș"
@@ -44,6 +60,7 @@ def test_results_page_pagination_not_existent_page(test_client):
     )
 
     assert response.status_code == 404
+
 
 def test_results_page_with_magazine_filter(test_client):
 
@@ -57,6 +74,7 @@ def test_results_page_with_magazine_filter(test_client):
 
     assert response.status_code == 200
 
+
 def test_results_page_with_magazine_filter_pagination(test_client):
 
     s_word = "Victor+Babeș"
@@ -65,10 +83,15 @@ def test_results_page_with_magazine_filter_pagination(test_client):
 
     response = test_client.get(
         "/results/search",
-        query_string={"magazine_filter": magazine_filter, "search_box": s_word, "page": page},
+        query_string={
+            "magazine_filter": magazine_filter,
+            "search_box": s_word,
+            "page": page,
+        },
     )
 
     assert response.status_code == 200
+
 
 def test_results_page_with_magazine_filter_go_back_buttons_displayed(test_client):
 
@@ -78,11 +101,16 @@ def test_results_page_with_magazine_filter_go_back_buttons_displayed(test_client
 
     response = test_client.get(
         "/results/search",
-        query_string={"magazine_filter": magazine_filter, "search_box": s_word, "page": page},
+        query_string={
+            "magazine_filter": magazine_filter,
+            "search_box": s_word,
+            "page": page,
+        },
     )
 
     assert b"Go back to home page" in response.data
     assert b"Go back to all results" in response.data
+
 
 def test_results_page_with_accepted_special_characters(test_client):
 
@@ -92,6 +120,7 @@ def test_results_page_with_accepted_special_characters(test_client):
     assert response.status_code == 200
     assert b"No results were found for" in response.data
 
+
 def test_results_page_with_unaccepted_special_characters(test_client):
 
     s_word = r"()&/\|~{}[]+=<>"
@@ -99,4 +128,52 @@ def test_results_page_with_unaccepted_special_characters(test_client):
     response = test_client.get("/results/search", query_string={"search_box": s_word})
 
     assert response.status_code == 200
-    assert b"The search term can have at least 4 characters and at most 200." in response.data
+    assert (
+        b"The search term can have at least 4 characters and at most 200."
+        in response.data
+    )
+
+
+# Tests for get_details_for_searched_term_for_specific_magazine
+class TestGetDetailsForSpecificMagazineForSearchedTerm:
+
+    def test_instance_of_get_details_for_specific_magazinese_for_searched_term(
+        self,
+        test_client,
+    ):
+
+        s_word = "Bucuresti"
+        # get a flask_sqlalchemy.query.Query object to be filtered
+        details_for_searched_term = get_details_for_searched_term(s_word)
+        magazine_filter = "Albina (1866-1876)"
+
+        details_for_searched_term_specific_magazine = (
+            get_details_for_searched_term_for_specific_magazine(
+                details_for_searched_term, magazine_filter
+            )
+        )
+
+        assert isinstance(
+            details_for_searched_term_specific_magazine, flask_sqlalchemy.query.Query
+        )
+
+    def test_filtering_works_for_get_details_for_searched_term_for_specific_magazine(
+        self,
+        test_client,
+    ):
+
+        s_word = "Bucuresti"
+        # get a flask_sqlalchemy.query.Query object to be filtered
+        details_for_searched_term = get_details_for_searched_term(s_word)
+        magazine_filter = "Albina (1866-1876)"
+
+        details_for_searched_term_for_specific_magazine = (
+            get_details_for_searched_term_for_specific_magazine(
+                details_for_searched_term, magazine_filter
+            )
+        )
+
+        assert len(list(details_for_searched_term_for_specific_magazine)) == 26
+
+        for row in details_for_searched_term_for_specific_magazine:
+            assert row[0] == magazine_filter
