139ef9d1e2d6ba1bf31aa490b2a900d24ff4f523
Andrei <Andrey@yahoo.com>
2024-06-14 11:41:40 +0300

    Re-ordered imports and formated docstrings

--- application/home/logic.py
+++ application/home/logic.py
@@ -1,39 +1,45 @@
-from ..models import (
+from application.models import (
     Magazines,
     MagazineDetails,
     db,
 )
+
 
 def get_existent_magazines():
     """
     Retrieve the names and ids of magazines from the Magazines table.
 
     Returns:
-        existent_magazines (flask_sqlalchemy.query.Query): A Query object containing
-        the retrieved names and ids.
+        existent_magazines (flask_sqlalchemy.query.Query): A Query object
+        containing the retrieved names and ids.
 
     The Query object can be iterated to access the results.
     """
 
-    existent_magazines = db.session.query(Magazines.name, Magazines.id).order_by(Magazines.name)
+    existent_magazines = db.session.query(Magazines.name, Magazines.id).order_by(
+        Magazines.name
+    )
     return existent_magazines
 
 
 def get_magazine_name(magazine_id=0):
     """
-    Retrieve the name of a magazine from the Magazines table based on the provided magazine_id.
+    Retrieve the name of a magazine from the Magazines table based on the
+    provided magazine_id.
 
     Args:
-        magazine_id (int, optional): The ID of the magazine to retrieve the name for. Defaults to 0.
+        magazine_id (int, optional): The ID of the magazine to retrieve the name
+        for. Defaults to 0.
 
     Returns:
-        magazine_name.name (str) or None: The name of the magazine if found, or None if the
-        magazine_id is not found or the magazine_id is of an invalid data type.
+        magazine_name.name (str) or None: The name of the magazine if found, or
+        None if the magazine_id is not found or the magazine_id is of an invalid
+        data type.
     """
 
     if not isinstance(magazine_id, int):
         return
-    
+
     try:
         magazine_name = db.session.get(Magazines, magazine_id)
     except OverflowError:
@@ -48,25 +54,25 @@ def get_magazine_details(magazine_id=0):
     Retrieve MagazineDetails instances for a given magazine_id.
 
     Args:
-        magazine_id (int, optional): The ID of the magazine to retrieve details for. Defaults to 0.
+        magazine_id (int, optional): The ID of the magazine to retrieve details
+        for. Defaults to 0.
 
     Returns:
-        magazine_details (flask_sqlalchemy.query.Query): A Query object containing MagazineDetails
-        instances with associated counts.
+        magazine_details (flask_sqlalchemy.query.Query): A Query object
+        containing MagazineDetails instances with associated counts.
 
-    This function returns a SQLAlchemy Query object that retrieves MagazineDetails instances for a given magazine_id.
-    Each MagazineDetails instance includes the distinct count of magazine numbers and pages associated with that year.
-    The Query object can be iterated to access the results.
-    If the magazine_id is not found or is of an invalid data type, the Query object will be empty.
+    This function returns a SQLAlchemy Query object that retrieves
+    MagazineDetails instances for a given magazine_id. Each MagazineDetails
+    instance includes the distinct count of magazine numbers and pages
+    associated with that year. The Query object can be iterated to access the
+    results. If the magazine_id is not found or is of an invalid data type, the
+    Query object will be empty.
     """
 
-    magazine_details = (
-    db.session.query(
+    magazine_details = db.session.query(
         MagazineDetails.year,
         MagazineDetails.distinct_magazine_numbers_count,
-        MagazineDetails.distinct_pages_count
-    )
-    .filter(MagazineDetails.magazine_id == magazine_id)
-    )
+        MagazineDetails.distinct_pages_count,
+    ).filter(MagazineDetails.magazine_id == magazine_id)
 
     return magazine_details
