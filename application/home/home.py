4d1a2f15beeb12d1c3ef5ae27ee3af7dc30d85df
Andrei <Andrey@yahoo.com>
2024-06-18 09:44:17 +0300

    Added log messages

--- application/home/home.py
+++ application/home/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, abort
+from flask import Blueprint, request, render_template, abort, current_app
 
 from application.forms import SearchForm
 from application.home.logic import (
@@ -16,6 +16,9 @@ home_bp = Blueprint(
 
 @home_bp.route("/", methods=["GET"])
 def search_form():
+
+    current_app.logger.info("Calling the search_form() function")
+
     search_form = SearchForm()
 
     existent_magazines = get_existent_magazines()
@@ -35,19 +38,39 @@ def search_form():
 @home_bp.route("/magazine_details")
 def show_magazine_details():
 
+    current_app.logger.info(
+        "Calling the show_magazine_details() function"
+        f" with parameter: {request.args.get('magazine_id')}"
+    )
+
     try:
         magazine_id = int(request.args.get("magazine_id"))
     except ValueError:
+        current_app.logger.error(
+            "Aborted show_magazine_details() function with ValueError error"
+            f" due to incorrect parameter: {request.args.get('magazine_id')}"
+        )
         abort(404)
     except TypeError:
+        current_app.logger.error(
+            "Aborted show_magazine_details() function with TypeError error"
+            f" due to incorrect parameter: {request.args.get('magazine_id')}"
+        )
         abort(404)
 
     magazine_name = get_magazine_name(magazine_id)
 
     if magazine_name is None:
+        current_app.logger.error(
+            "Aborted show_magazine_details() function becuase magazine_name was"
+            f" not found for parameter: {request.args.get('magazine_id')}"
+        )
         abort(404)
 
     magazine_details = get_magazine_details(magazine_id)
+    current_app.logger.info(
+        f"Displaying the /magazine_details page for magazine_name: {magazine_name}"
+    )
 
     return render_template(
         "magazine_details.html",
@@ -58,9 +81,11 @@ def show_magazine_details():
 
 @home_bp.route("/contact")
 def show_contact_page():
+    current_app.logger.info("Calling the show_contact_page() function")
     return render_template("contact.html")
 
 
 @home_bp.route("/about")
 def show_about_page():
+    current_app.logger.info("Calling the show_about_page() function")
     return render_template("about.html")
