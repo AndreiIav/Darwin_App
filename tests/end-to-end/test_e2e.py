72e50dfb0d04fb8b4676fb59dc5452b03c690b7f
Andrei <Andrey@yahoo.com>
2024-06-27 11:15:21 +0300

    Added test_home_clicking_on_magazine_name_opens_magazine_details() and test_home_searching_with_valid_term_opens_search_page() tests

--- tests/end-to-end/test_e2e.py
+++ tests/end-to-end/test_e2e.py
@@ -37,3 +37,27 @@ def test_home_locate_all_elements(page, start_app_server):
     expect(
         page.locator("form").filter(has_text="Amicul Şcoalei (1925-1935)")
     ).to_be_visible()
+
+
+def test_home_clicking_on_magazine_name_opens_magazine_details(page, start_app_server):
+
+    start_page = start_app_server
+
+    page.goto(start_page)
+    with page.expect_popup() as new_page:
+        page.get_by_role("button", name="Albina (1866-1876)").click()
+    magazine_details_page = new_page.value
+
+    expect(magazine_details_page).to_have_title(" Albina (1866-1876)-BCU-Search ")
+
+
+def test_home_searching_with_valid_term_opens_search_page(page, start_app_server):
+
+    start_page = start_app_server
+
+    page.goto(start_page)
+    page.get_by_placeholder("the search term can have at").click()
+    page.get_by_placeholder("the search term can have at").fill("bucuresti")
+    page.get_by_placeholder("the search term can have at").press("Enter")
+
+    expect(page).to_have_title(" Search Results-BCU-Search ")
