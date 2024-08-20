f94762876d2f02d97e9a956b810e48d8d13c7d8b
Andrei <Andrey@yahoo.com>
2024-08-20 10:24:09 +0300

    Refactor: added cache for distinct_magazines_and_count

--- application/search_page/search_page_routes.py
+++ application/search_page/search_page_routes.py
@@ -62,6 +62,20 @@ def search_for_term():
         formatted_s_word=formatted_s_word
     )
 
+    distinct_magazines_and_count_cache_key = (
+        f"{formatted_s_word}_distinct_magazines_count"
+    )
+    if cache.has(distinct_magazines_and_count_cache_key):
+        distinct_magazines_and_count = cache.get(distinct_magazines_and_count_cache_key)
+    else:
+        # cast Query object to list to make possible to add to cache
+        distinct_magazines_and_count = list(
+            get_distinct_magazine_names_and_count_for_searched_term(
+                details_for_searched_term=details_for_searched_term
+            )
+        )
+        cache.add(distinct_magazines_and_count_cache_key, distinct_magazines_and_count)
+
     # count the lenght of the entire result set only if a magazine_filter
     # doesn't exists
     if not magazine_filter:
@@ -78,13 +92,7 @@ def search_for_term():
             )
             return render_template("no_results_found.html", searched_term=s_word)
 
-    distinct_magazines_and_count = (
-        get_distinct_magazine_names_and_count_for_searched_term(
-            details_for_searched_term=details_for_searched_term
-        )
-    )
-
-    if magazine_filter:
+    else:
         current_app.logger.info(f"magazine_filter set to: {magazine_filter}")
         details_for_searched_term = get_details_for_searched_term_for_specific_magazine(
             details_for_searched_term, magazine_filter
